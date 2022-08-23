d1b13daa42db22f6692e3c6e8fdeff3fd2057545
ayoife00 <olalereolatunbosun@gmail.com>
2022-08-23 09:04:34 -0700

    Write a for loop to check

--- task2/test.js
+++ task2/test.js
@@ -4,6 +4,10 @@ const numbers =[-5,-2,-6,0,-1]
 //DECLARE A VARIABLE TO CHECK FOR THE HIGHEST 
 let highest = 0;
 
-
-
-
+//Write a for loop
+for(let i=0; i<numbers.length; i++){
+    if (numbers[i] > highest){
+        highest = numbers[i]
+    }
+} 
+console.log(highest);
